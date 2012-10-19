bc72bfe40819a02ce88671458ccb019fc4eb8f82
Greg Ward <greg@gerg.ca>
2012-10-19 17:03:49 -0400

    lexer: refactor tests

--- src/fubsy/fulex_test.go
+++ src/fubsy/fulex_test.go
@@ -4,28 +4,25 @@ import (
 	"testing"
 )
 
-func TestScan_valid(t *testing.T) {
-	scanner := NewScanner("nofile", []byte("]  [\"foo!bar\"\n ]"))
-	scanner.scan()
+func TestScan_valid_1(t *testing.T) {
+	input := "]  [\"foo!bar\"\n ]"
 	expect := []toktext{
 		{"nofile", 1, ']', "]"},
 		{"nofile", 1, '[', "["},
 		{"nofile", 1, QSTRING, "\"foo!bar\""},
 		{"nofile", 2, ']', "]"},
 	}
-	checkTokens(t, expect, scanner.tokens)
+	assertScan(t, expect, "nofile", input)
 }
 
 func TestScan_inline_1(t *testing.T) {
-	scanner := NewScanner(
-		"blop.txt", []byte(" \n{{{yo\nhello\nthere\n}}}"))
-	scanner.scan()
+	input := " \n{{{yo\nhello\nthere\n}}}"
 	expect := []toktext{
 		{"blop.txt", 2, L3BRACE, "{{{"},
 		{"blop.txt", 2, INLINE, "yo\nhello\nthere\n"},
 		{"blop.txt", 5, R3BRACE, "}}}"},
 	}
-	checkTokens(t, expect, scanner.tokens)
+	assertScan(t, expect, "blop.txt", input)
 }
 
 func TestScan_inline_2(t *testing.T) {
@@ -33,38 +30,33 @@ func TestScan_inline_2(t *testing.T) {
 	// was newline (or indeed anything at all) after }}} -- I just
 	// threw a bunch of punctuation into the inline text to be sure
 	// that works too
-	scanner := NewScanner(
-		"blop.txt", []byte("{{{ any!chars\"are\nallowed'here\n}}}\n"))
-	scanner.scan()
+	input := "{{{ any!chars\"are\nallowed'here\n}}}\n"
 	expect := []toktext{
 		{"blop.txt", 1, L3BRACE, "{{{"},
 		{"blop.txt", 1, INLINE, " any!chars\"are\nallowed'here\n"},
 		{"blop.txt", 3, R3BRACE, "}}}"},
 	}
-	checkTokens(t, expect, scanner.tokens)
+	assertScan(t, expect, "blop.txt", input)
 }
 
 func TestScan_inline_open(t *testing.T) {
 	// bad input: unclosed {{{ (should be a syntax error, not an
 	// infinite loop!) (hmmm: would be nice to report the trailing
 	// inline contents as a BADTOKEN; might give a better syntax error)
-	scanner := NewScanner("foo", []byte("] {{{bip\nbop!["))
-	scanner.scan()
+	input := "] {{{bip\nbop!["
 	expect := []toktext{
 		{"foo", 1, ']', "]"},
 		{"foo", 1, L3BRACE, "{{{"},
 	}
-	checkTokens(t, expect, scanner.tokens)
+	assertScan(t, expect, "foo", input)
 
 	// same result on incomplete }}}
-	scanner = NewScanner("foo", append(scanner.input, "\n}}"...))
-	scanner.scan()
-	checkTokens(t, expect, scanner.tokens)
+	input += "\n}}"
+	assertScan(t, expect, "foo", input)
 }
 
 func TestScan_invalid(t *testing.T) {
-	scanner := NewScanner("fwob", []byte("]]\n!-\"whee]\" x whizz\nbang"))
-	scanner.scan()
+	input := "]]\n!-\"whee]\" x whizz\nbang"
 	expect := []toktext{
 		{"fwob", 1, ']', "]"},
 		{"fwob", 1, ']', "]"},
@@ -74,10 +66,16 @@ func TestScan_invalid(t *testing.T) {
 		{"fwob", 2, BADTOKEN, "whizz"},
 		{"fwob", 3, BADTOKEN, "bang"},
 		}
-	checkTokens(t, expect, scanner.tokens)
+	assertScan(t, expect, "fwob", input)
 }
 
-func checkTokens(t *testing.T, expect []toktext, actual []toktext) {
+func assertScan(t *testing.T, expect []toktext, filename string, input string) {
+	scanner := NewScanner(filename, []byte(input))
+	scanner.scan()
+	assertTokens(t, expect, scanner.tokens)
+}
+
+func assertTokens(t *testing.T, expect []toktext, actual []toktext) {
 	if len(expect) != len(actual) {
 		t.Fatalf("expected %d tokens, but got %d",
 			len(expect), len(actual))
