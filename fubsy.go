e9ca0efe1df18c8026a09c4b36b85800bba447e3
Greg Ward <greg@gerg.ca>
2012-11-19 15:22:37 -0500

    main: report runtime errors too (just like parse errors)

--- fubsy.go
+++ fubsy.go
@@ -18,15 +18,20 @@ func main() {
 	if ast == nil && len(errors) == 0 {
 		panic("ast == nil && len(errors) == 0")
 	}
-	if len(errors) > 0 {
-		for _, err := range errors {
-	 		fmt.Fprintln(os.Stderr, "parse error:", err)
-		}
-	 	os.Exit(1)
-	}
+	checkErrors("parse error:", errors)
 	fmt.Printf("ast:\n")
 	ast.Dump(os.Stdout, "")
 
 	rt := runtime.NewRuntime(script, ast)
-	rt.RunScript()
+	errors = rt.RunScript()
+	checkErrors("error:", errors)
+}
+
+func checkErrors(prefix string, errors []error) {
+	if len(errors) > 0 {
+		for _, err := range errors {
+	 		fmt.Fprintln(os.Stderr, prefix, err)
+		}
+	 	os.Exit(1)
+	}
 }
