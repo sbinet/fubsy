f99db3a4c4c55748612d7365f895e5ecd365be7a
Greg Ward <greg@gerg.ca>
2012-12-12 16:54:18 -0500

    doc: add custom stylesheet

--- doc/guide/conf.py
+++ doc/guide/conf.py
@@ -67,10 +67,11 @@ html_theme_options = {
     'linkcolor': '#0000d0',
     'bodyfont': '"Helvetica Neue", Helvetica, Arial, sans-serif',
     'headfont': '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    'codebgcolor': '#f0f0a0',
+    'codetextcolor': 'black',
     }
 
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_style = 'fubsy.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -91,7 +92,7 @@ html_title = "Fubsy User Guide"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
