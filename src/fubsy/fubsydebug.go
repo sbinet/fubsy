b8f72253acf598a89abbd7f29dcb8c7fc04439b9
Greg Ward <greg@gerg.ca>
2013-01-22 22:10:12 -0500

    add missing copyright statements

--- src/fubsy/fubsydebug.go
+++ src/fubsy/fubsydebug.go
@@ -1,3 +1,7 @@
+// Copyright © 2013, Greg Ward. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE.txt file.
+
 package main
 
 // Debug utility for Fubsy. Invoked via the main fubsy binary, but
